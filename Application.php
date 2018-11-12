491dacdd18c52c3139234fad8a1a7258098f7100
Mike White <mike.wildcats@gmail.com>
2018-11-12 16:19:35 -0500

    Initial Application

---
+++ Application.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @brief		EverPanel Application Class
+ * @author		<a href='http://everneth.com'>TptMike</a>
+ * @copyright	(c) 2018 TptMike
+ * @package		Invision Community
+ * @subpackage	EverPanel
+ * @since		12 Nov 2018
+ * @version		
+ */
+ 
+namespace IPS\everpanel;
+
+/**
+ * EverPanel Application Class
+ */
+class _Application extends \IPS\Application
+{
+	
+}
